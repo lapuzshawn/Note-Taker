4ac605e76d4121d8f0fa526e693260b5cfad7b59
Shawn Lapuz <lapuzshawn@gmail.com>
2023-05-23 20:42:43 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up body parsing, static, and route middleware
+// Set up body parsing, static, and route middleware, 
+// for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static('public')); // Serve static files from the 'public' directory
+
+
+app.use('/api', apiRoutes); // Route middleware for API routes
+app.use('/', htmlRoutes); // Route middleware for HTML routes
+
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
