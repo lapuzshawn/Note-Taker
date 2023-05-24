e0766133ad151a012906397e0b71964ce2e6f320
Shawn Lapuz <lapuzshawn@gmail.com>
2023-05-23 20:29:34 -0700

    Completed API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const store = require('../db/store');
 
-// GET "/api/notes" responds with all notes from the database
+// GET "/api/notes" gets all notes from DB
 router.get('/notes', async (req, res) => {
   try {
     const notes = await store.getNotes();
@@ -11,7 +11,7 @@ router.get('/notes', async (req, res) => {
   }
 });
 
-// POST "/api/notes" adds a new note to the database
+// POST "/api/notes" adds a new note to the DB
 router.post('/notes', async (req, res) => {
   try {
     const note = await store.addNote(req.body);
@@ -21,7 +21,7 @@ router.post('/notes', async (req, res) => {
   }
 });
 
-// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+// DELETE "/api/notes" deletes the note 
 router.delete('/notes/:id', async (req, res) => {
   try {
     await store.removeNote(req.params.id);
